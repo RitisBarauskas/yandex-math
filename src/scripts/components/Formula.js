a47565b9caf719f6d415bc3c8187601e3fc0d807
Ritis <perm300ru@yandex.ru>
2021-08-20 01:17:39 +0500

    доработана анимация стрелок

--- src/scripts/components/Formula.js
+++ src/scripts/components/Formula.js
@@ -1,6 +1,7 @@
 class Formula {
     constructor(page) {
         this.page = page;
+        this._text = document.querySelector('.content__text');
         this._formula = document.querySelector('.formula');
         this._arrowAX = this._formula.querySelector('.formula__arrow-ax');
         this._arrowAY = this._formula.querySelector('.formula__arrow-ay');
@@ -31,12 +32,16 @@ class Formula {
     update(page) {
         if (page === 1) {
             this._buttonBack.style.display = 'none';
+            this._text.style.color = '#fff';
             this._buttonRefresh.style.display = 'none';
             this._buttonNext.style.display = 'block';
-            this._arrowAX.style.display = 'none';
-            this._arrowAY.style.display = 'none';
-            this._arrowBX.style.display = 'none';
-            this._arrowBY.style.display = 'none';
+            this._arrowAX.classList.remove('formula__arrow-ax_show');
+            this._arrowAY.classList.remove('formula__arrow-ay_show');
+            this._arrowAY.classList.remove('formula__arrow-ay_show-reverse');
+            this._arrowBX.classList.remove('formula__arrow-bx_show');
+            this._arrowBX.classList.remove('formula__arrow-bx_show-reverse');
+            this._arrowBY.classList.remove('formula__arrow-by_show');
+            this._arrowBY.classList.remove('formula__arrow-by_show-reverse');
             this._secondA.classList.remove('formula__second-a_show-reverse');
             this._secondA.classList.remove('formula__second-a_show');
             this._secondA.classList.remove('formula__second-x_show-reverse');
@@ -57,7 +62,7 @@ class Formula {
             this._thirdY.classList.remove('formula__third-y_show');
         } else if (page === 2) {
             this._buttonBack.style.display = 'block';
-            this._arrowAX.style.display = 'block';
+            this._arrowAX.classList.add('formula__arrow-ax_show');
             this._secondEqual.classList.remove('formula__second-equal_show');
             if (this._secondA.classList.contains('formula__second-a_show')) {
                 this._secondA.classList.remove('formula__second-a_show');
@@ -81,9 +86,15 @@ class Formula {
             this._secondX.classList.add('formula__second-x_show');
             this._firstPlus.classList.add('formula__first-plus_show');
             this._firstPoints.classList.add('formula__first-points_show');
-            this._arrowAY.style.display = 'none';
+            if (this._arrowAY.classList.contains('formula__arrow-ay_show')) {
+                this._arrowAY.classList.remove('formula__arrow-ay_show')
+                this._arrowAY.classList.add('formula__arrow-ay_show-reverse');
+            }
         } else if (page === 4) {
-            this._arrowAY.style.display = 'block';
+            if (this._arrowAY.classList.contains('formula__arrow-ay_show-reverse')) {
+                this._arrowAY.classList.remove('formula__arrow-ay_show-reverse');
+            }
+            this._arrowAY.classList.add('formula__arrow-ay_show');
             this._firstPoints.classList.add('formula__first-points_show');
             if (this._thirdA.classList.contains('formula__third-a_show')) {
                 this._thirdA.classList.remove('formula__third-a_show');
@@ -107,13 +118,19 @@ class Formula {
             this._secondY.classList.add('formula__second-y_show');
             this._secondPlus.classList.add('formula__second-plus_show');
             this._secondPoints.classList.add('formula__second-points_show');
-            this._arrowAY.style.display = 'block';
-            this._arrowAX.style.display = 'block';
-            this._arrowBX.style.display = 'none';
+            this._arrowAY.classList.add('formula__arrow-ay_show');
+            this._arrowAX.classList.add('formula__arrow-ax_show');
+            if (this._arrowBX.classList.contains('formula__arrow-bx_show')) {
+                this._arrowBX.classList.remove('formula__arrow-bx_show');
+                this._arrowBX.classList.add('formula__arrow-bx_show-reverse');
+            }
         } else if (page === 6) {
-            this._arrowAY.style.display = 'none';
-            this._arrowAX.style.display = 'none';
-            this._arrowBX.style.display = 'block';
+            this._arrowAY.classList.remove('formula__arrow-ay_show');
+            this._arrowAX.classList.remove('formula__arrow-ax_show');
+            if (this._arrowBX.classList.contains('formula__arrow-bx_show-reverse')) {
+                this._arrowBX.classList.remove('formula__arrow-bx_show-reverse');
+            }
+            this._arrowBX.classList.add('formula__arrow-bx_show');
             this._secondPoints.classList.add('formula__second-points_show');
             if (this._secondB.classList.contains('formula__second-b_show')) {
                 this._secondB.classList.remove('formula__second-b_show');
@@ -137,13 +154,21 @@ class Formula {
             this._thirdX.classList.add('formula__third-x_show');
             this._thirdPlus.classList.add('formula__third-plus_show');
             this._thirdPoints.classList.add('formula__third-points_show');
-            this._arrowBY.style.display = 'none';
+            if (this._arrowBY.classList.contains('formula__arrow-by_show')) {
+                this._arrowBY.classList.add('formula__arrow-by_show-reverse');
+                this._arrowBY.classList.remove('formula__arrow-by_show');
+            }
         } else if (page === 8) {
-            this._arrowBY.style.display = 'block';
+            this._arrowBX.classList.add('formula__arrow-bx_show');
+            if (this._arrowBY.classList.contains('formula__arrow-by_show-reverse')) {
+                this._arrowBY.classList.remove('formula__arrow-by_show-reverse');
+            }
+            this._arrowBY.classList.add('formula__arrow-by_show');
             this._thirdPoints.classList.add('formula__third-points_show');
         } else if (page === 9) {
-            this._arrowBX.style.display = 'none';
-            this._arrowBY.style.display = 'none';
+            this._text.style.color = '#1A1B22';
+            this._arrowBX.classList.remove('formula__arrow-bx_show');
+            this._arrowBY.classList.remove('formula__arrow-by_show');
             this._thirdPoints.classList.remove('formula__third-points_show');
             this._thirdB.classList.add('formula__third-b_show');
             this._thirdY.classList.add('formula__third-y_show');
